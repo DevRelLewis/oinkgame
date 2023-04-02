27b3999b06ea98e0a416698d9c047800b4257b0b
Lewis Meyers <meyerslewis193@gmail.com>
2023-04-02 13:21:10 -0400

    revert commit

--- script.js
+++ script.js
@@ -93,7 +93,6 @@ btnHold.addEventListener('click', function () {
       // Finish game
       // If not, switch player to the next player
       switchPlayer();
-      console.log(switchPlayer);
     }
   }
 });
